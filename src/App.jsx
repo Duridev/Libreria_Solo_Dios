91208f62a477897e98541e96cd3efd2561942c6c
Daniel Uribe <duribegalaz@gmail.com>
2023-05-04 00:03:41 -0400

    Creacion de componente Vista para cambio de modo oscuro/claro

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import ContenedorProducto from './components/ContenedorProductos/ContenedorProdu
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
+import Vista from './components/Vista/Vista';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Vista />
       <ItemListContainer greeting="Hay libros que parecen casi vivos por su poder para transmitir, no sólo ideas, sino un nuevo estado de conciencia."/>
       <ItemCount />
       <ContenedorProducto />
