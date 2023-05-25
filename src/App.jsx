ce7b47fce67bf25c91df346167a7a8d6ef481d3c
Daniel Uribe <duribegalaz@gmail.com>
2023-05-24 23:43:21 -0400

    creacion funcion getProductosPorCategoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import './App.css';
-//import Producto from './components/Producto/Producto';
-//import ContenedorProducto from './components/ContenedorProductos/ContenedorProducto';
 import NavBar from './components/NavBar/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-//import ItemCount from './components/ItemCount/ItemCount';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
   return (
     <>
-      <NavBar />
-      <ItemListContainer greeting="Hay libros que parecen casi vivos por su poder para transmitir, no sólo ideas, sino un nuevo estado de conciencia."/>
-      <ItemDetailContainer/>
-      {/* <ItemCount />
-      <ContenedorProducto /> */}
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={ <ItemListContainer/> }/>
+          <Route path='/categorias/:idCategorias' element={ <ItemListContainer/> }/>
+          <Route path='/item/:idItem' element={ <ItemDetailContainer/> }/>
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,7 +1,8 @@
 const productos = [
-    {id: 1, img: "./img/autobiografia-de-un-yogui.jpg", nombre: "Autobiografia De Un Yogui", autor: "Paramhansa Yogananda", editorial: "Crystal Clarity Publishers", precio: 20000, paginas: 562},
-    {id: 2, img: "./img/secretos-revelados.jpg", nombre: "Secretos Revelados de Kriya Yoga", autor: "JC Stevens", editorial: "Golden Swan Publishing", precio: 30000, paginas: 376},
-    {id: 3, img: "./img/el-nuevo-sendero.jpg", nombre: "El Nuevo Sendero", autor: "Swami Kriyananda", editorial: "Asociación Ananda Ediciones", precio: 20000, paginas: 626}
+    {id: 1, img: "./img/autobiografia-de-un-yogui.jpg", nombre: "Autobiografia De Un Yogui", autor: "Paramhansa Yogananda", editorial: "Crystal Clarity Publishers", precio: 20000, paginas: 562, idCat:"2"},
+    {id: 2, img: "./img/secretos-revelados.jpg", nombre: "Secretos Revelados de Kriya Yoga", autor: "JC Stevens", editorial: "Golden Swan Publishing", precio: 30000, paginas: 376, idCat:"2"},
+    {id: 3, img: "./img/el-nuevo-sendero.jpg", nombre: "El Nuevo Sendero", autor: "Swami Kriyananda", editorial: "Asociación Ananda Ediciones", precio: 20000, paginas: 626, idCat:"3"},
+    {id: 3, img: "./img/solodios.jpg", nombre: "Sólo Dios", autor: "Self-Realization Fellowship", editorial: "Self-Realization Fellowship", precio: 13000, paginas: 341, idCat:"3"}
 ];
 
 export const getProductos = () => {
@@ -21,3 +22,12 @@ export const getUnProducto = (id) => {
         }, 2000)
     })
 }
+
+export const getProductosPorCategoria = (idCategoria) => {
+    return new Promise( resolve => {
+        setTimeout( () => {
+            const productosCategoria = productos.filter(prod => prod.idCat === idCategoria)
+            resolve(productosCategoria);
+        },  100 )
+    })
+} 
