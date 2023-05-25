9dc440584dec014671c533a165e81e6034bf2fc8
Daniel Uribe <duribegalaz@gmail.com>
2023-05-25 02:28:10 -0400

    detalles visuales

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,7 +5,6 @@ const ItemDetail = ({img, id, nombre,autor,editorial,precio,paginas}) => {
         <div className='contenedorItem'>
             <img className='imgProducto' src={img} alt={nombre}/>
             <h3>Nombre: {nombre}</h3>
-            <p>Id: {id}</p>
             <p>Autor: {autor}</p>
             <p>Editorial: {editorial}</p>
             <p>Precio: {precio}</p>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,18 +5,18 @@ import { NavLink, Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <header>
-        <Link to="/">
+        <Link to="/" className="link">
             <h1>Librería Solo Dios</h1>
         </Link>
 
         <nav>
             <ul>
-                <li><NavLink to="/">Nuestros Libros</NavLink></li>
+                <li><NavLink to="/" className="link">Nuestros Libros</NavLink></li>
                 <li>Autores</li>
-                <li><NavLink to="/categoria/1">Paramahansa Yogananda</NavLink></li>
-                <li><NavLink to="/categoria/2">Swami Kriyananda</NavLink></li>
-                <li><NavLink to="/categoria/3">Self-Realization Fellowship</NavLink></li>
-                <li><NavLink to="/categoria/4">Otros Autores</NavLink></li>
+                <li><NavLink to="/categoria/1" className="link">Paramahansa Yogananda</NavLink></li>
+                <li><NavLink to="/categoria/2" className="link">Swami Kriyananda</NavLink></li>
+                <li><NavLink to="/categoria/3" className="link">Self-Realization Fellowship</NavLink></li>
+                <li><NavLink to="/categoria/4" className="link">Otros Autores</NavLink></li>
             </ul>
         </nav>
 
