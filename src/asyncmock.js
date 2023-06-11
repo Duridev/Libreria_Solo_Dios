22481362e395301a20477f2daafa5f694da4cfbb
Daniel Uribe <duribegalaz@gmail.com>
2023-06-11 00:17:54 -0400

    Contador Sincronizado

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,12 +1,12 @@
 const productos = [
-    {id: "1", img: "../img/autobiografia-de-un-yogui.jpg", nombre: "Autobiografia De Un Yogui", autor: "Paramhansa Yogananda", editorial: "Crystal Clarity Publishers", precio: 20000, paginas: 562, idCat:"1"},
-    {id: "2", img: "../img/secretos-revelados.jpg", nombre: "Secretos Revelados de Kriya Yoga", autor: "JC Stevens", editorial: "Golden Swan Publishing", precio: 30000, paginas: 376, idCat:"4"},
-    {id: "3", img: "../img/el-nuevo-sendero.jpg", nombre: "El Nuevo Sendero", autor: "Swami Kriyananda", editorial: "Ananda Ediciones", precio: 20000, paginas: 626, idCat:"2"},
-    {id: "4", img: "../img/solodios.jpg", nombre: "Sólo Dios", autor: "Self-Realization Fellowship", editorial: "Self-Realization Fellowship", precio: 13000, paginas: 341, idCat:"3"},
-    {id: "5", img: "../img/raja-yoga-el-manual-completo-de-yoga-y-meditacion.jpg", nombre: "Raya Yoga", autor: "Swami Kriyananda", editorial: "Ananda Ediciones", precio: 25000, paginas: 442, idCat:"2"},
-    {id: "6", img: "../img/elvinomistico.jpg", nombre: "El vino del místico", autor: "Paramahansa Yogananda", editorial: "Self-Realization Fellowship", precio: 20000, paginas: 280, idCat:"1"},
-    {id: "7", img: "../img/dosranas.jpg", nombre: "Dos Ranas en Apuros", autor: "Paramahansa Yogananda", editorial: "Self-Realization Fellowship", precio: 26000, paginas: 26, idCat:"1"},
-    {id: "8", img: "../img/rajarsi.jpg", nombre: "Rajarsi Janakananda", autor: "Self-Realization Fellowship", editorial: "Self-Realization Fellowship", precio: 12000, paginas: 304, idCat:"3"},
+    {id: "1", img: "../img/autobiografia-de-un-yogui.jpg", nombre: "Autobiografia De Un Yogui", autor: "Paramhansa Yogananda", editorial: "Crystal Clarity Publishers", precio: 20000, stock: 5, paginas: 562, idCat:"1"},
+    {id: "2", img: "../img/secretos-revelados.jpg", nombre: "Secretos Revelados de Kriya Yoga", autor: "JC Stevens", editorial: "Golden Swan Publishing", precio: 30000, stock: 10, paginas: 376, idCat:"4"},
+    {id: "3", img: "../img/el-nuevo-sendero.jpg", nombre: "El Nuevo Sendero", autor: "Swami Kriyananda", editorial: "Ananda Ediciones", precio: 20000, stock: 10, paginas: 626, idCat:"2"},
+    {id: "4", img: "../img/solodios.jpg", nombre: "Sólo Dios", autor: "Self-Realization Fellowship", editorial: "Self-Realization Fellowship", precio: 13000, stock: 10, paginas: 341, idCat:"3"},
+    {id: "5", img: "../img/raja-yoga-el-manual-completo-de-yoga-y-meditacion.jpg", nombre: "Raya Yoga", autor: "Swami Kriyananda", editorial: "Ananda Ediciones", precio: 25000, stock: 10, paginas: 442, idCat:"2"},
+    {id: "6", img: "../img/elvinomistico.jpg", nombre: "El vino del místico", autor: "Paramahansa Yogananda", editorial: "Self-Realization Fellowship", precio: 20000, stock: 10, paginas: 280, idCat:"1"},
+    {id: "7", img: "../img/dosranas.jpg", nombre: "Dos Ranas en Apuros", autor: "Paramahansa Yogananda", editorial: "Self-Realization Fellowship", precio: 26000, stock: 10, paginas: 26, idCat:"1"},
+    {id: "8", img: "../img/rajarsi.jpg", nombre: "Rajarsi Janakananda", autor: "Self-Realization Fellowship", editorial: "Self-Realization Fellowship", precio: 12000, stock: 10, paginas: 304, idCat:"3"},
 ];
 
 export const getProductos = () => {
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,15 @@
 import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const ItemDetail = ({img, id, nombre,autor,editorial,precio,paginas}) => {
+const ItemDetail = ({img, id, nombre,autor,editorial,precio,paginas, stock}) => {
+    const [agregarCantidad, setAgregarCantidad] = useState(0);
+
+    const manejadorCantidad = (cantidad) => {
+        setAgregarCantidad(cantidad);
+        console.log("Productos Agregardos:", + cantidad);
+    }
     return (
         <div className='contenedorItem'>
             <img className='imgProducto' src={img} alt={nombre}/>
@@ -9,6 +18,9 @@ const ItemDetail = ({img, id, nombre,autor,editorial,precio,paginas}) => {
             <p>Editorial: {editorial}</p>
             <p>Precio: {precio}</p>
             <p>Páginas: {paginas}</p>
+            {
+                agregarCantidad > 0 ? (<Link to="/cart">Terminar Compra</Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad}/>)
+            }
         </div>
     )
 }
