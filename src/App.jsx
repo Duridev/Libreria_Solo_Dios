776c5e66297f5c8b4c26833563df6cf022593cc4
Daniel Uribe <duribegalaz@gmail.com>
2023-06-17 00:07:04 -0400

    se realiza el Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext';
 import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
 
 
 
@@ -21,6 +22,7 @@ function App() {
             <Route path='/item/:idItem' element={ <ItemDetailContainer/> }/>
             <Route path='*' element={<h2>Sitio en Construcción</h2>}/>
             <Route path='/cart' element = {<Cart/>} />
+            <Route path='/checkout' element={ <Checkout/>} />
           </Routes>
         </CarritoProvider>
       </BrowserRouter>
